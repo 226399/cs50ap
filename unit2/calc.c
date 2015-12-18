3217d1b5a1da5c4f812fed4ce7d434b1dfa1af9d
Jeff Maxim <ubuntu@065262ce3c91>
2015-12-18 12:55:09 +0000

    pset 2-3

---
+++ unit2/calc.c
@@ -0,0 +1,53 @@
+// includes
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    //check correcty argc
+    if (argc != 4) {
+        printf("usage: ./calc <number> <operand> <number>\n");
+        return 1;
+    }
+    
+    //check if argv[2] is appropriate operand
+    if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != 'x' && argv[2][0] != '/' && argv[2][0] != '%') {
+        printf("what the heck kind of operand is that. Getchur math straight, son!");
+        return 1;
+    }
+    
+    //get nums from argv
+    float firstNum = atof(argv[1]);
+    float secondNum = atof(argv[3]);
+    
+    //get operand from argv
+    string operandString = argv[2];
+    char operandChar = operandString[0];
+    
+    //initialize result
+    float result;
+    int bigNum;
+    
+    //perform operation
+    switch(operandChar) {
+        case '+' :
+            result = firstNum + secondNum;
+            break;
+        case '-' :
+            result = firstNum - secondNum;
+            break;
+        case 'x' :
+            result = firstNum * secondNum;
+            break;
+        case '/' :
+            result = firstNum / secondNum;
+            break;
+        case '%':
+            bigNum = (int)(firstNum / secondNum);
+            result = firstNum - (secondNum * bigNum);
+    }
+    
+    //print result
+    printf("Result: %.6f\n", result);
+}
